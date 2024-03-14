9ccd4c53d9ae0814f1a3d0127cc39608499613f9
Spandan Barve <spandan567@gmail.com>
2024-03-15 02:25:10 +0530

    correctify content

--- src/pages/HomePage/components/Hero.tsx
+++ src/pages/HomePage/components/Hero.tsx
@@ -19,16 +19,19 @@ export default function Hero() {
   }, [tumblers]);
 
   return (
-    <section className="h-screen flex justify-between p-page relative overflow-hidden">
-      <div className="flex flex-col gap-y-10 items-center justify-center">
+    <section className="h-screen flex justify-between p-page-lg relative overflow-hidden">
+      <div className="flex flex-col gap-y-10 items-center justify-center text-center">
         <h1 className="text-7xl">Vutaleek Beetrootin</h1>
         <p className="text-center text-2xl">
-          I'M $PENGIE
-          <br /> Everyone knows me as PENG's wife
+          Vutaleek is a valorous sir and that gent shall restore peace and order
+          on eth'reum
         </p>
       </div>
       <div className="">
-        <img src="/images/vitalik-self-hd.png" className="h-full py-[3vh]" />
+        <img
+          src="/images/ICON-hd.png"
+          className="h-full object-contain py-[3vh]"
+        />
       </div>
 
       <div className="absolute-cover -z-1">{tumblers}</div>
